aef069f837039db89e26793030dc45b4b9aa8062
mishradhirajmishra <dhiraj.siswa.mishra@gmail.com>
2019-09-24 22:00:27 +0530

    first commit

---
+++ controller/admin.js
@@ -0,0 +1,85 @@
+const con = require('../database/connection');
+
+module.exports.index=(req,res)=>{
+    query= "select * from event";
+    con.query(query,(err,result)=>{
+        console.log(result);
+        res.render('index', { title: 'All Events', events :result });
+    })
+}
+
+module.exports.addGet=(req,res)=>{
+    res.render('addEvent', { title: 'Add Event' });
+}
+
+module.exports.addPost=(req,res)=>{
+
+    var name = req.body.name;
+    var date = req.body.date;
+    if (req.files && Object.keys(req.files).length != 0) {
+  
+        var image = req.files.image.name;
+        /*=====================================*/
+        var dir = "./public/images/"+image;
+        sampleFile = req.files.image;
+        sampleFile.mv(dir, function(err) {
+            if (err)
+                return res.status(500).send(err);
+        });
+        /*=====================================*/
+        var query ="INSERT INTO event (name,image,date) VALUES ('"+name+"','"+image+"','"+date+"')";
+ 
+    }else{
+        var query ="INSERT INTO event (name,date) VALUES ('"+name+"','"+date+"')";
+       
+    
+    }
+    con.query(query,function (err,result) {
+        res.redirect('http://localhost:3000');
+    });
+}
+
+module.exports.editGet=(req,res)=>{
+    var id = req.params.id;
+    var query="select * from event where id= '"+id+"'";
+    con.query(query,(err,result)=>{
+        console.log(result)
+        res.render('editEvent', { title: 'Update Event', event:result[0] });
+    })
+}
+
+module.exports.updatePost=(req,res)=>{
+    var id = req.body.id;
+    var name = req.body.name;
+    var date = req.body.date;
+    
+    if (req.files && Object.keys(req.files).length != 0) {
+  
+        var image = req.files.image.name;
+        /*=====================================*/
+        var dir = "./public/images/"+image;
+        sampleFile = req.files.image;
+        sampleFile.mv(dir, function(err) {
+            if (err)
+                return res.status(500).send(err);
+        });
+        /*=====================================*/
+        var query ="Update event SET name='"+name+"',image='"+image+"',date='"+date+"'where id='"+id+"'";
+ 
+    }else{
+        var query ="Update event SET name='"+name+"',date='"+date+"'where id='"+id+"'";
+       
+    
+    }
+    con.query(query,function (err,result) {
+        res.redirect('http://localhost:3000');
+    });
+}
+
+module.exports.delete=(req,res,next)=>{
+    var id = req.body.id;
+    var query="delete from event where id = '"+id+"'"
+    con.query(query,(err,result)=>{    
+    });
+    next();
+}
---
+++ routes/router.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const cont = require('../controller/admin');
+/* GET home page. */
+router.get('/', cont.index );
+router.get('/add', cont.addGet );
+router.post('/', cont.addPost );
+router.get('/edit/:id', cont.editGet );
+router.post('/update', cont.updatePost );
+router.delete('/',cont.delete,cont.index );
+module.exports = router;
